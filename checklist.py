cae681296c34b70feefa40bcb01164a4a90240b9
squealy1273 <padynriddell@Padyns-MacBook-Pro.local>
2019-08-28 17:18:10 -0700

    Added checklist.py

---
+++ checklist.py
@@ -0,0 +1,36 @@
+#print("Hello World")
+# Create
+checklist = list()
+checklist.append('Blue')
+print(checklist)
+checklist.append('Orange')
+print(checklist
+
+# Read
+#checklist[0]
+return checklist[0]
+
+# Update
+checklist = ['Blue', 'Orange']
+checklist[1] = 'Cats'
+print(checklist)
+
+# Destroy
+checklist = ['Blue', 'Cats']
+checklist.pop(1)
+print(checklist)
+
+
+#my_fun_function('Hello World')
+def test():
+create("purple sox")
+create("red cloak")
+
+print(read(0))
+print(read(1))
+
+update(0, "purple socks")
+destory(1)
+
+print(read(0))
+print(read(1))
