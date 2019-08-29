6b0d979753212b45dd371f27f07df9f1bd5d5f42
squealy1273 <padynriddell@Padyns-MacBook-Pro.local>
2019-08-29 16:21:55 -0700

    Added list items

--- checklist.py
+++ checklist.py
@@ -1,36 +1,71 @@
-#print("Hello World")
+print("Hello World")
+#my_fun_function('Hello World')
+
+
 # Create
-checklist = list()
-checklist.append('Blue')
-print(checklist)
-checklist.append('Orange')
-print(checklist
+#checklist = list()
+#checklist.append('Blue')
+#print(checklist)
+#checklist.append('Orange')
+#print(checklist
 
 # Read
 #checklist[0]
-return checklist[0]
+#return checklist[0]
 
 # Update
-checklist = ['Blue', 'Orange']
-checklist[1] = 'Cats'
-print(checklist)
+#checklist[1] = 'Cats'
+#print(checklist)
 
 # Destroy
-checklist = ['Blue', 'Cats']
-checklist.pop(1)
-print(checklist)
+#checklist = ['Blue', 'Cats']
+#print(checklist)
 
 
-#my_fun_function('Hello World')
+
+
+# Create our checklist
+checklist = list()
+
+# Define Functions
+def create(item):
+    create("purple socks")
+    create("red cloak")
+
+def read(item):
+    print(read(0))
+    print(read(1))
+
+def update(index, item):
+    update(0, "purple socks")
+
+def destroy(index):
+    destroy(1)
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(index + list_item)
+        index += 1
+#def mark_completed(index):
+
+
+#def select(function_code):
+
+#def user_input(prompt):
+
 def test():
-create("purple sox")
-create("red cloak")
+    create("purple socks")
+    create("red cloak")
 
-print(read(0))
-print(read(1))
+    print(read(0))
+    print(read(1))
 
-update(0, "purple socks")
-destory(1)
+    update(0, "purple socks")
 
-print(read(0))
-print(read(1))
+    destroy(1)
+
+    print(read(0))
+
+    list_all_items()
+    print("{} {}".format(index, list_item))
