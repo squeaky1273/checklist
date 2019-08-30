c78dc90dacac581cd2569324306e8d1b8f8940ef
squealy1273 <padynriddell@Padyns-MacBook-Pro.local>
2019-08-30 15:52:23 -0700

    Filled in print function for place

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,8 @@
 name = input("Enter your name: ")
+place = input("Enter a place: ")
 noun = input("Enter a noun: ")
 adjective = input("Enter an adjective: ")
 
 
 print("Hi, my name is " + name)
-print("")
+print("I was on my way to " + place)
