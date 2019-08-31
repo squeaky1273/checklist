d9bd4aaddc843e5d02f17e8b5029b7d47936a0f0
squealy1273 <padynriddell@Padyns-MacBook-Pro.local>
2019-08-31 11:13:25 -0700

    Added more parts of speech

--- madlibs.py
+++ madlibs.py
@@ -1,14 +1,17 @@
 
 inputs = ['name', '']
 #Create
+interjection = input("Enter an interjection (Expressive word): ")
 name = input("Enter your name: ")
 place = input("Enter a place: ")
 verb = input("Enter a verb ending in -ing: ")
 noun = input("Enter a noun: ")
-#adjective = input("Enter an adjective: ")
+adjective = input("Enter an adjective: ")
+color = input("Enter a color: ")
 
 #Read
-print("Hi, my name is " + name + ". I have a weird story to tell you. ")
+print(interjection + "! My name is " + name + ". I have a weird story to tell you. ")
 print("I was on my way to the " + place + ".")
 print("While " + verb + ", I saw a truck. ")
-print("the truck that I saw was so particular. I reminded me of a " + noun + ".")
+print("The truck that I saw was so peculiar because it reminded me of a " + noun + ".")
+print("The wheels were " + adjective + " and the whole thing was the color, " + color + ".")
