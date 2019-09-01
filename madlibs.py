631d541d5e434cd585c21ea104aa808ad628dc8e
squealy1273 <padynriddell@Padyns-MBP.hsd1.ca.comcast.net>
2019-08-31 17:50:32 -0700

    Added functions

--- madlibs.py
+++ madlibs.py
@@ -1,17 +1,18 @@
+def create():
+    inputs = ['interjection', 'name', 'place', 'verb', 'noun', 'adjective', 'color']
 
-inputs = ['name', '']
-#Create
-interjection = input("Enter an interjection (Expressive word): ")
-name = input("Enter your name: ")
-place = input("Enter a place: ")
-verb = input("Enter a verb ending in -ing: ")
-noun = input("Enter a noun: ")
-adjective = input("Enter an adjective: ")
-color = input("Enter a color: ")
-
-#Read
-print(interjection + "! My name is " + name + ". I have a weird story to tell you. ")
-print("I was on my way to the " + place + ".")
-print("While " + verb + ", I saw a truck. ")
-print("The truck that I saw was so peculiar because it reminded me of a " + noun + ".")
-print("The wheels were " + adjective + " and the whole thing was the color, " + color + ".")
+    interjection = input("Enter an interjection (expressive word): ")
+    name = input("Enter your name: ")
+    place = input("Enter a place: ")
+    veb = input("Enter a verb ending in -ing")
+    noun = input("Enter a noun: ")
+    adjective = input("Enter an adjective: ")
+    color = input("Enter a color: ")
+create()
+def read():
+    print(inputs(0) + "! My name is " + inputs(1) + ". I have a weird story to tell you. ")
+    print("I was on my way to the " + inputs(2) + ".")
+    print("While " + inputs(3) + ", I saw a truck. ")
+    print("The truck that I saw was so peculiar because it reminded me of a " + inputs(4) + ".")
+    print("The wheels were " + inputs(5) + " and the whole thing was the color, " + inputs(6) + ".")
+read()
