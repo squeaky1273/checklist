bf3559053b64bba3149e6b3e25e6e85412c00012
squealy1273 <padynriddell@Padyns-MacBook-Pro.local>
2019-08-31 21:07:02 -0700

    Updated functions #1

--- madlibs.py
+++ madlibs.py
@@ -1,18 +1,16 @@
-def create():
-    inputs = ['interjection', 'name', 'place', 'verb', 'noun', 'adjective', 'color']
+words = ['interjection', 'name', 'place', 'verb', 'noun', 'adjective', 'color']
+def test():
+    words[0] = input("Enter an interjection: ")
+    words[1] = input("Enter a name: ")
+    words[2] = input("Enter a place: ")
+    words[3] = input("Enter a verb ending in -ing: ")
+    words[4] = input("Enter a noun: ")
+    words[5] = input("Enter an adjective: ")
+    words[6] = input("Enter a color: ")
 
-    interjection = input("Enter an interjection (expressive word): ")
-    name = input("Enter your name: ")
-    place = input("Enter a place: ")
-    veb = input("Enter a verb ending in -ing")
-    noun = input("Enter a noun: ")
-    adjective = input("Enter an adjective: ")
-    color = input("Enter a color: ")
-create()
-def read():
-    print(inputs(0) + "! My name is " + inputs(1) + ". I have a weird story to tell you. ")
-    print("I was on my way to the " + inputs(2) + ".")
-    print("While " + inputs(3) + ", I saw a truck. ")
-    print("The truck that I saw was so peculiar because it reminded me of a " + inputs(4) + ".")
-    print("The wheels were " + inputs(5) + " and the whole thing was the color, " + inputs(6) + ".")
-read()
+    print(words[0] + "! My name is " + words[1] + ". I have a weird story to tell you. ")
+    print("I was on my way to the " + words[2] + ".")
+    print("While " + words[3] + ", I saw a truck. ")
+    print("The truck that I saw was so peculiar because it reminded me of a " + words[4] + ".")
+    print("The wheels were " + words[5] + " and the whole thing was the color, " + words[6] + ".")
+test()
